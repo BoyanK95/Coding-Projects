db471e1bf22fdf657df3f08b7f5c4c885a8d7850
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-08-17 02:40:39 +0300

    Created an ingredient server

--- NodeDemo/firstApi/server.js
+++ NodeDemo/firstApi/server.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+var ingredients = [
+    {
+        "id":"232kAk",
+        "text":"Eggs"
+    },
+    {
+        "id":"dkP352",
+        "text":"Milk"
+    },
+    {
+        "id":"dkcuuu7",
+        "text":"Bacon"
+    },
+    {
+        "id":"73hdy",
+        "text":"Frogs Legs"
+    }
+];
+
 
 app.get('/', function(request, response) {
-    response.send('My First API!');
+    response.send(ingredients);
 });
 
 app.listen(3000, function() {
