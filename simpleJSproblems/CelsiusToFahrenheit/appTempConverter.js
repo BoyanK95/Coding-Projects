cebf4a758a013ba2bca610518c164842dedf1530
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-08-25 01:30:23 +0300

    Temp-converter

---
+++ simpleJSproblems/CelsiusToFahrenheit/appTempConverter.js
@@ -0,0 +1,11 @@
+let temp = 32;
+temp = toFahrenheit(temp);
+console.log(temp);
+
+function toCelsius(temp) {
+   return (temp -32) * (5/9);
+}
+
+function toFahrenheit(temp) {
+    return temp * 9 / 5 + 32;
+}
