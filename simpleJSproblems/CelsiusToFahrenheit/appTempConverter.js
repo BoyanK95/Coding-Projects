8f421ed53e9e887d1e3e5be71ff25d1e426cce9a
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-08-25 18:50:37 +0300

    TempConverter
    Finished the JS behind the Temp Converter !

--- simpleJSproblems/CelsiusToFahrenheit/appTempConverter.js
+++ simpleJSproblems/CelsiusToFahrenheit/appTempConverter.js
@@ -1,6 +1,31 @@
-let temp = 32;
-temp = toFahrenheit(temp);
-console.log(temp);
+document.getElementById("submitButton").onclick = function() {
+    
+let temp;
+
+if(document.getElementById("cButton").checked){
+    temp = document.getElementById("textBox").value;
+    temp = Number(temp);
+    temp = toCelsius(temp);
+    document.getElementById("tempLabel").innerHTML = temp + "°";
+}
+else if(document.getElementById("fButton").checked) {
+    temp = document.getElementById("textBox").value;
+    temp = Number(temp);
+    temp = toFahrenheit(temp);
+    document.getElementById("tempLabel").innerHTML = temp +"°";
+
+}
+else {
+    alert("Select a unit!");
+    document.getElementById("tempLabel").innerHTML = "Select a unit!";
+}
+
+
+}
+
+
+
+
 
 function toCelsius(temp) {
    return (temp -32) * (5/9);
