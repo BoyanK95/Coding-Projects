5dc55fd80f490140aa9de40805db6026864f30bb
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-08-30 03:25:35 +0300

    PopUpScam Site

--- simpleJSproblems/PopUps/setTimeou.js
+++ simpleJSproblems/PopUps/setTimeou.js
@@ -1,17 +1,29 @@
-setTimeout(firstMessage, 3000);
-setTimeout(secondMessage, 6000);
-setTimeout(thirdMessage, 9000);
+let item = "cryptocyrrency";
+let price = 420.69;
+
+
+let timer1 = setTimeout(firstMessage, 3000, item, price);
+let timer2 = setTimeout(secondMessage, 6000);
+let timer3 = setTimeout(thirdMessage, 9000);
 
 
 
-function firstMessage() {
-    alert(`Buy this course for $500!`)
+function firstMessage(item, price) {
+    alert(`Buy this ${item} for $${price} !`);
 }
 
 function secondMessage() {
-    alert(`This is not a scam!`)
+    alert(`This is not a scam!`);
 }
 
 function thirdMessage() {
-    alert(`Do ITT!!!`)
+    alert(`Do ITT!!!`);
+}
+
+
+document.getElementById("myBtn").onclick = function () {
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    clearTimeout(timer3);
+    alert(`Thanks for buying <3`);
 }
