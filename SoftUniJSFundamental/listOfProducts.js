110afa2aa4562d003a95cc26ee87239802740ab8
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-09-15 03:35:27 +0300

    some Js functions

--- SoftUniJSFundamental/listOfProducts.js
+++ SoftUniJSFundamental/listOfProducts.js
@@ -1,6 +1,6 @@
 function list(arr) {
   let sorted = arr.sort();
-  console.log(sorted);
+
   for (let i = 0; i < sorted.length; i++) {
     let product = sorted[i];
     console.log(`${i + 1}.${product}`);
