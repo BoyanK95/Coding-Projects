59a6a41c868b13707f727a5e537e7a5df7380a21
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-09-20 21:34:28 +0300

    JS functions
    Some JS functions from my JS Advance, Objects and composition: exercise

---
+++ SoftUni Advanced/lection8ObjExcercice/ConstructionCrew.js
@@ -0,0 +1,12 @@
+function constructionCrew(input) {
+
+
+    
+}
+
+constructionCrew({
+  weight: 80,
+  experience: 1,
+  levelOfHydrated: 0,
+  dizziness: true,
+});
--- SoftUni Advanced/lecture7/twonPopulation.js
+++ SoftUni Advanced/lecture7/twonPopulation.js
@@ -1,17 +1,24 @@
 function solve(input) {
-    let towns = []
-    input.forEach(line => {
-        let token = line.split(' <->');
-        let name = token[0]
-        let population = token[1]
-        towns.push({name, population})
-    });
-    
-    
+  const towns = {};
+  for (let line of input) {
+    let [name, population] = line.split(" <-> ");
+    population = Number(population);
+
+    if (towns[name] != undefined) {
+      population += towns[name];
+    }
+    towns[name] = population;
+  }
+  for (let town in towns) {
+    console.log(`${town} : ${towns[town]}`);
+  }
 }
 
-solve(['Sofia <-> 1200000',
-'Montana <-> 20000',
-'New York <-> 10000000',
-'Washington <-> 2345000',
-'Las Vegas <-> 1000000'])
+solve([
+  "Sofia <-> 1200000",
+  "Montana <-> 20000",
+  "New York <-> 10000000",
+  "Washington <-> 2345000",
+  "Las Vegas <-> 1000000",
+]);
+
