279c74e10a08796843e37e935efe7fcb5fd57a2a
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-09-21 15:34:26 +0300

    JS functions
    Some JS functions from my softuni advance Objects lecture

--- SoftUni Advanced/Lecture4/cookingByNumbers.js
+++ SoftUni Advanced/Lecture4/cookingByNumbers.js
@@ -31,3 +31,5 @@ cooking(
 
   "fillet"
 );
+
+
--- SoftUni Advanced/lecture7/demo.js
+++ SoftUni Advanced/lecture7/demo.js
@@ -12,3 +12,16 @@ let otherObj = {
 let newStudent = Object.assign(student, otherObj)
 
 console.log(newStudent);
+
+
+const compareNumber = {
+    ascending: (a, b) => a - b,
+    descending: (a, b) => b - a
+};
+
+
+let numbers = [1, 2, 5, 7, 32, 532, 63,777];
+
+numbers.sort(compareNumber.descending);
+
+console.log(numbers);
