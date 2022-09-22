5c81b51f54452ddade72cde855c06212314d5ed4
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-09-22 03:29:24 +0300

    Some JS functions
    Some functions from my JS advance course

--- SoftUni Advanced/lection9DOMintro/DOMproblems/01.Edit Element/EditElement.js
+++ SoftUni Advanced/lection9DOMintro/DOMproblems/01.Edit Element/EditElement.js
@@ -1,3 +1,14 @@
-function editElement(ref, matcher, replacer) {
-    
+function editElement(element, match, replacer) {
+  while (element.textContent.includes(match)) {
+    element.textContent = element.textContent.replace(match, replacer);
+  }
+}
+
+// Regex solution
+function regeXeditElement(element, match, replacer) {
+
+const pattern = new RegExp(match, 'g')
+
+element.textContent = element.textContent.replace(pattern, replacer);
+
 }
--- SoftUni Advanced/lection9DOMintro/DOMproblems/02.Collect List Items/CollectListItems.js
+++ SoftUni Advanced/lection9DOMintro/DOMproblems/02.Collect List Items/CollectListItems.js
@@ -1,6 +1,16 @@
 function extractText() {
-    let listItems = document.querySelectorAll('li')
-    let resultForm = document.getElementById('#result')
+    // let listItems = document.querySelectorAll('li')
+    // let resultForm = document.getElementById('result')
 
     
+    // for (let list of listItems) {
+    //    resultForm.innerHTML += list.innerHTML;
+       
+    // }
+
+    let ulElement = document.getElementById('items');
+    let result = document.getElementById('result');
+
+
+    result.textContent = ulElement.textContent
 }
