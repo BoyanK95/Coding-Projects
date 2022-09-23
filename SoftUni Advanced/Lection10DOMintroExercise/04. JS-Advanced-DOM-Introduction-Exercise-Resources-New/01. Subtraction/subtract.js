157d45e149dd9f935a3dcc4ec6e7926f68e2fa63
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-09-23 17:14:54 +0300

    CSS Exercise
    some CSS exercises from my JS advance module in softuni

--- SoftUni Advanced/Lection10DOMintroExercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
+++ SoftUni Advanced/Lection10DOMintroExercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -1,16 +1,13 @@
 function subtract() {
-    let num1 = document.getElementById('firstNumber').value;
-    let num2 = document.getElementById('secondNumber').value;
-    const result = document.getElementById('result')
-    num1 = Number(num1);
-    num2 = Number(num2);
+  let num1 = document.getElementById("firstNumber").value;
+  let num2 = document.getElementById("secondNumber").value;
+  const result = document.getElementById("result");
+  num1 = Number(num1);
+  num2 = Number(num2);
 
-    let subtraction = num1 - num2;
+  let subtraction = num1 - num2;
 
-    result.innerHTML = subtraction
-    
-    
+  result.innerHTML = subtraction;
 
-
-    console.log(result);
+  console.log(result);
 }
