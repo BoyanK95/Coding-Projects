a0a0fa1547afa47270eda132eaf0dc2b6509c57c
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-23 19:48:44 +0300

    Date

---
+++ w3Problems/1stProblem/dateApp.js
@@ -0,0 +1,30 @@
+const currentDateEl = document.getElementById('date')
+const currentDayEl = document.getElementById('day')
+const today = new Date();
+const day = today.getDay()
+const daylist = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Friday', 'Saturday']
+let todayValue = 'Today is : ' + daylist[day] + '.';
+currentDayEl.textContent = todayValue
+let hour = today.getHours();
+let minute = today.getMinutes();
+let second = today.getSeconds();
+let prepand = (hour >= 12)? 'PM' : 'AM';
+hour = (hour >= 12) ? hour - 12: hour;
+if (hour === 0 && prepand === 'PM') {
+    if (minute === 0 && second === 0) {
+        hour = 12
+        prepand = 'Noon'
+    }else{
+        hour = 12
+        prepand = ' PM'
+    }
+}
+
+if (hour === 0 && prepand === ' AM') {
+    if (minute=== 0 && second === 0) {
+        hour = 12
+        prepand= ' AM'
+    }
+}
+console.log('Current Time : ' + hour + prepand + ' : ' + minute + ' : ' + second);
+currentDateEl.textContent = `Current Time : ${hour} ${prepand} : ${minute} : ${second}`
